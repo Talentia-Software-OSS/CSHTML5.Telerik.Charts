0bdaee9eca26de584ecae49064349006bc3f82a4
CSHTML5 <contact@cshtml5.com>
2019-08-09 16:39:08 +0200

    Simplify code in the demo app to retain only what's really useful

--- CSHTML5.Telerik.Charts/CSHTML5.TelerikChartExample/MainPage.xaml.cs
+++ CSHTML5.Telerik.Charts/CSHTML5.TelerikChartExample/MainPage.xaml.cs
@@ -12,48 +12,51 @@ namespace CSHTML5.TelerikChartExample
 {
     public partial class MainPage
     {
-        #region Unused
-        internal kendo_ui_chart.kendo.dataviz.ui.Chart _unsedChart;
-        #endregion
+        internal kendo_ui_chart.kendo.dataviz.ui.Chart _unsedChart; // Keep this line because it ensures that the startup project has a direct reference to the Kendo Chart project.
 
-        static Random random = new Random();
-        #region variables
         public class DTPoint
         {
             public DateTime XValue { get; set; }
             public double YValue { get; set; }
         }
-        public enum PeriodType
-        {
-            Day,
-            Week,
-            Month,
-            Year,
-        }
 
-        private Dictionary<DateTime, int> dateDictionary = new Dictionary<DateTime, int>();
+        static Random random = new Random();
         private RadObservableCollection<DTPoint> centerAvgGraph = new RadObservableCollection<DTPoint>();
-        private int[] sourceIndices;
-        private int sourceSize, sourceLeft, sourceRight, bufferSize, bufferLeft, bufferRight;
-        private float sourceIncrement;
-        private const int WINDOW_SIZE = 300; // used if aggregating
-        private KeyValuePair<DateTime, double>[] averages;
-        private float MaxScale, MinScale, previousSliderValue;
+        private float MaxScale, MinScale;
         private double MaxY;
-        private bool lastDateSetting = false, usingNonGraphicMethod = false;
 
-        #endregion
-
-        #region start
         public MainPage()
         {
-            this.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-            this.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
             InitializeComponent();
             Loaded += SetReferencePeriod_Loaded;
         }
 
-        private RadObservableCollection<DTPoint> GenerateRandomSerie(int size, DateTime initialDateTime, double minValue = 0, double maxValue = 300, int intervalBetweenPoints = 1, PeriodType intervalType = PeriodType.Month)
+        private void SetReferencePeriod_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReferenceGraph.Series[0].ItemsSource = centerAvgGraph;
+            MaxScale = 300;
+            MaxY = MaxScale + 10.0;
+            displayGraph();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            displayGraph();
+        }
+
+        private void displayGraph()
+        {
+            centerAvgGraph.Clear();
+            centerAvgGraph = GenerateRandomSerie(10, new DateTime(2000, 1, 1));
+            XAxis.MajorTickInterval = 2;
+            MaxY = MaxScale + 10.0;
+            YAxis.Maximum = MaxY;
+            ReferenceGraph.Series[0].ItemsSource = centerAvgGraph;
+            ReferenceGraph.Series[1].ItemsSource = GenerateRandomSerie(5, new DateTime(2000, 1, 1));
+            ReferenceGraph.Series[2].ItemsSource = GenerateRandomSerie(5, new DateTime(2000, 6, 1));
+        }
+
+        private RadObservableCollection<DTPoint> GenerateRandomSerie(int size, DateTime initialDateTime, double minValue = 0, double maxValue = 300, int intervalBetweenPoints = 1)
         {
             RadObservableCollection<DTPoint> serie = new RadObservableCollection<DTPoint>();
             DateTime currentDate = initialDateTime;
@@ -66,95 +69,10 @@ namespace CSHTML5.TelerikChartExample
                     YValue = rd.NextDouble() * (maxValue - minValue) + minValue,
                 };
                 serie.Add(point);
-                switch (intervalType)
-                {
-                    case PeriodType.Day:
-                        currentDate = currentDate.AddDays(intervalBetweenPoints);
-                        break;
-                    case PeriodType.Week:
-                        currentDate = currentDate.AddDays(intervalBetweenPoints * 7);
-                        break;
-                    case PeriodType.Month:
-                        currentDate = currentDate.AddMonths(intervalBetweenPoints);
-                        break;
-                    case PeriodType.Year:
-                        currentDate = currentDate.AddYears(intervalBetweenPoints);
-                        break;
-                }
+                currentDate = currentDate.AddMonths(intervalBetweenPoints);
             }
             return serie;
         }
-
-        private void SetReferencePeriod_Loaded(object sender, RoutedEventArgs e)
-        {
-            ReferenceGraph.Series[0].ItemsSource = centerAvgGraph;
-            MaxScale = 300;
-            MaxY = MaxScale + 10.0;
-            fillBuffers();
-            ReferenceGraph.Visibility = System.Windows.Visibility.Visible;
-            Focus();
-        }
-        #endregion
-
-        private void fillBuffers()
-        {
-            int sourceIndex, bufferIndex;
-            int windowSize = sourceRight - sourceLeft + 1;
-            bufferSize = windowSize;
-            averages = new KeyValuePair<DateTime, double>[bufferSize];
-            sourceIndices = new int[bufferSize];
-            for (bufferIndex = 0, sourceIndex = sourceLeft; sourceIndex <= sourceRight; sourceIndex++, bufferIndex++)
-            {
-                sourceIndices[bufferIndex] = sourceIndex;
-            }
-            sourceIncrement = 1F;
-
-            bufferLeft = 0;
-            bufferRight = bufferSize - 1;
-            centerAvgGraph.Clear();
-            fillGraph(!usingNonGraphicMethod);
-        }
-
-        private void fillGraph(bool useBufferDates = true)
-        {
-            centerAvgGraph = GenerateRandomSerie(10, new DateTime(2000, 1, 1));
-            displayGraph(useBufferDates);
-        }
-
-        private void setYAxis()
-        {
-            MaxY = MaxScale + 10.0;
-            YAxis.Maximum = MaxY;
-        }
-
-        private void setXAxis()
-        {
-            XAxis.MajorTickInterval = 2;
-        }
-
-        private void displayGraph(bool useBufferDates = true)
-        {
-            lastDateSetting = useBufferDates;
-            setXAxis();
-            setYAxis();
-            ReferenceGraph.Series[0].ItemsSource = centerAvgGraph;
-            SetBackgroundRanges();
-        }
-
-        private void SetBackgroundRanges()
-        {
-            ReferenceGraph.Series[1].ItemsSource = GenerateRandomSerie(5, new DateTime(2000, 1, 1));
-            ReferenceGraph.Series[2].ItemsSource = GenerateRandomSerie(5, new DateTime(2000, 6, 1));
-        }
-
-        #region apply non graphics selection method
-        private void ApplyNonGraphicSelectionMethod_Click(object sender, RoutedEventArgs e)
-        {
-            usingNonGraphicMethod = true;
-            fillBuffers();
-            usingNonGraphicMethod = false;
-        }
-        #endregion
     }
 }
 
