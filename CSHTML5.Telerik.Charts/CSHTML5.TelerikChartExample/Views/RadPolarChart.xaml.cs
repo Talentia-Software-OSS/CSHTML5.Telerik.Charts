a98f9a320962602be9401db393b7cf749fed4b06
ralucacolta <41778243+ralucacolta@users.noreply.github.com>
2021-06-29 18:11:06 +0300

    added polar chart type polarLine (#2)

---
+++ CSHTML5.Telerik.Charts/CSHTML5.TelerikChartExample/Views/RadPolarChart.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace CSHTML5.TelerikChartExample.Views
+{
+    public partial class RadPolarChart : Page
+    {
+        public RadPolarChart()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click_PolarChart(object sender, RoutedEventArgs e)
+        {
+            // set piechart data on refresh
+            ExamplePolarChart.Refresh();
+        }
+    }
+}
