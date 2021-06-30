e5dae057098efcca9079cbefda5b67b35fcb45bd
mvoitescu <67779992+mvoitescu@users.noreply.github.com>
2021-06-30 10:14:14 +0300

    Solved merging problems

--- CSHTML5.Telerik.Charts/CSHTML5.TelerikChartExample/Views/RadPieChart.xaml.cs
+++ CSHTML5.Telerik.Charts/CSHTML5.TelerikChartExample/Views/RadPieChart.xaml.cs
@@ -26,111 +26,34 @@ namespace CSHTML5.TelerikChartExample.Views
 
         private ChartViewModel _chartViewModel;
 
-    // TODO: remove chart info
-    public class ChartInfo
-    {
-        public string Label { get; set; }
-        public double Value { get; set; }
-        public string Color { get; set; }
-    }
-
-    public class PlannedTimeByCycleChartViewModel : INotifyCollectionChanged, INotifyPropertyChanged
-    {
-        #region NotifyPropertyChanged
-        event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string propertyName)
+        public RadPieChart()
         {
             this.InitializeComponent();
             var items = GetSerie();
             _chartViewModel = new ChartViewModel(this, items);
         }
-        #endregion
 
-
-        
-        //#region DependencyProperty
-
-        //public static readonly DependencyProperty HoursByCycleDataProperty = DependencyProperty.Register("HoursByCycle", typeof(ObservableCollection<ChartInfo>), typeof(PlannedTimeByCycleChartViewModel), new PropertyMetadata(null));
-
-        //#endregion
-
-        //#region Fields
-        //public RadObservableCollection<ChartInfo> HoursByCycle
-        //{
-        //    get
-        //    {
-        //        return (RadObservableCollection<ChartInfo>)this.GetValue(HoursByCycleDataProperty);
-        //    }
-        //    set
-        //    {
-        //        this.SetValue(HoursByCycleDataProperty, value);
-        //    }
-        //}
-        //#endregion
-
-
-        private RadObservableCollection<ChartInfo> _items;
-
-        /// <summary>
-        /// Gets or sets the process contexts.
-        /// </summary>
-        /// <value>The process contexts.</value>
-        public RadObservableCollection<ChartInfo> Items
+        private void Button_Click_PieChart(object sender, RoutedEventArgs e)
         {
-             // set piechart data on refresh
+            // set piechart data on refresh
             ExamplePieChart.Refresh();
         }
 
         private void AddItems_Click(object sender, RoutedEventArgs e)
-        {            
+        {
             _chartViewModel.Items.Add(new PieSerieObj() { Category = "North africa", Value = 13.6, Color = "#033939" });
         }
 
         private static RadObservableCollection<PieSerieObj> GetSerie()
         {
-            NotifyCollectionChangedEventHandler handler = this.CollectionChanged;
-            if (handler != null)
-            {
-                handler(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-            }
-        }
+            RadObservableCollection<PieSerieObj> serie = new RadObservableCollection<PieSerieObj>();
 
-        /// <summary>
-        /// Occurs when the items list of the collection has changed, or the collection is reset.
-        /// </summary>
-        public event NotifyCollectionChangedEventHandler CollectionChanged;
-
-
-        #region Constructors
-        public PlannedTimeByCycleChartViewModel()
-        {
-            Items = GetSerie();
-        }
-
-        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
-        {
-            add
-            {
-                throw new NotImplementedException();
-            }
-
-            remove
-            {
-                throw new NotImplementedException();
-            }
-        }
-        #endregion
-
-        public static RadObservableCollection<ChartInfo> GetSerie()
-        {
-            RadObservableCollection<ChartInfo> serie = new RadObservableCollection<ChartInfo>();
-
-            serie.Add(new ChartInfo() { Label = "Asia", Value = 53.8, Color = "#9de219" });
-            serie.Add(new ChartInfo() { Label = "Europe", Value = 16.1, Color = "#90cc38" });
-            serie.Add(new ChartInfo() { Label = "Latin America", Value = 11.3, Color = "#068c35" });
-            serie.Add(new ChartInfo() { Label = "Africa", Value = 9.6, Color = "#006634" });
-            serie.Add(new ChartInfo() { Label = "Middle East", Value = 5.2, Color = "#004d38" });
-            serie.Add(new ChartInfo() { Label = "North America", Value = 3.6, Color = "#033939" });
+            serie.Add(new PieSerieObj() { Category = "Asia", Value = 53.8, Color = "#9de219" });
+            serie.Add(new PieSerieObj() { Category = "Europe", Value = 16.1, Color = "#90cc38" });
+            serie.Add(new PieSerieObj() { Category = "Latin America", Value = 11.3, Color = "#068c35" });
+            serie.Add(new PieSerieObj() { Category = "Africa", Value = 9.6, Color = "#006634" });
+            serie.Add(new PieSerieObj() { Category = "Middle East", Value = 5.2, Color = "#004d38" });
+            serie.Add(new PieSerieObj() { Category = "North America", Value = 3.6, Color = "#033939" });
 
             return serie;
         }
