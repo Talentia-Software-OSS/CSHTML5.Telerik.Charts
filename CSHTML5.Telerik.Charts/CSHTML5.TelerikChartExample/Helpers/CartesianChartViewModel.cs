7df9cfd07d2b8a76ced7df76da3166f5c6f381b5
mvoitescu <mihail.voitescu@addedo.ch>
2021-07-08 13:14:40 +0300

    Applied the propert viewmodel binding to carthesian chart

---
+++ CSHTML5.Telerik.Charts/CSHTML5.TelerikChartExample/Helpers/CartesianChartViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using Telerik.Windows.Data;
+
+namespace CSHTML5.TelerikChartExample.Views
+{
+    public class CartesianChartViewModel<TChartPage, TChartSeries> : PolarChartViewModel<TChartPage, TChartSeries>
+        where TChartPage : Page
+    {
+        RadObservableCollection<TChartSeries> _series4 = new RadObservableCollection<TChartSeries>();
+        public RadObservableCollection<TChartSeries> Series4
+        {
+            get { return _series4; }
+            set { _series4 = value; OnPropertyChanged("Series4"); }
+        }
+
+        public CartesianChartViewModel(
+            TChartPage page, 
+            RadObservableCollection<TChartSeries> series1,
+            RadObservableCollection<TChartSeries> series2,
+            RadObservableCollection<TChartSeries> series3,
+            RadObservableCollection<TChartSeries> series4
+        ) : base(page, series1, series2, series3)
+        {
+            this.Series4 = series4;
+        }
+    }
+}
