be5fac7592d06018d3621998627047cc55206269
andivlad-talentia <67757358+andivlad-talentia@users.noreply.github.com>
2021-09-28 19:13:03 +0200

    Update to beta (#19)

---
+++ CSHTML5.Telerik.Charts/TestChartApplication/TestChartApplication.Simulator/Startup.cs
@@ -0,0 +1,15 @@
+﻿using CSHTML5.TelerikChartExample;
+using OpenSilver.Simulator;
+using System;
+
+namespace TestChartApplication.Simulator
+{
+    static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
